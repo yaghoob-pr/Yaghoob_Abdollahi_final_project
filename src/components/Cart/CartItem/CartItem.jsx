e3070ca9c47d8d60b6e884dfa7fbf9161150e82f
yaghoob-pr <yaghoob.silvana14@gmail.com>
2021-08-20 18:21:49 +0430

    create and and CartItem component

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Typography, Card, Button, CardActions, CardContent, CardMedia} from '@material-ui/core'
+import useStyles from './styles'
+
+const CartItem = ({item}) => {
+    const classes = useStyles();
+
+    return (
+        <Card>
+            <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+            <CardContent className={classes.cardContent}>
+                <Typography variant='h4'>{item.name}</Typography>
+                <Typography variant='h5'>{item.line_total.formatted}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <div className={classes.buttons}>
+                    <Button type='button' size='small'>-</Button>
+                    <Typography>{item.quantity}</Typography>{}
+                    <Button type='button' size='small'>+</Button>
+                    </div>
+                    <Button variant='contained' type='button' color='secondary'>حذف</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+
+export default CartItem
